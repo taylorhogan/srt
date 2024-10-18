5335238edfca2f9ab28aba95bdcd0daf567c34aa
taylorhogan <taylor.hogan@gmail.com>
2024-10-18 13:44:16 -0400

    work on start/end stuff

---
+++ start.py
@@ -0,0 +1,53 @@
+import asyncio
+from collections import OrderedDict
+
+from kasa import Discover
+
+_super_user_config = OrderedDict(
+    {
+        "Super Users": {
+            'Thogan', 'tmhobservatory'
+        },
+    })
+
+
+async def doit():
+    ip = _super_user_config["name_map"]["Telescope mount"]
+    dev = await Discover.discover_single(ip)
+    await dev.turn_on()
+    await dev.update()
+
+    ip = _super_user_config["name_map"]["Iris back lights"]
+    dev = await Discover.discover_single(ip)
+    await dev.turn_off()
+    await dev.update()
+
+    ip = _super_user_config["name_map"]["Iris door light"]
+    dev = await Discover.discover_single(ip)
+    await dev.turn_off()
+    await dev.update()
+
+    ip = _super_user_config["name_map"]["Iris inside light"]
+    dev = await Discover.discover_single(ip)
+    await dev.turn_off()
+    await dev.update()
+
+    ip = _super_user_config["name_map"]["Driveway lights"]
+    dev = await Discover.discover_single(ip)
+    await dev.turn_off()
+    await dev.update()
+
+
+async def make_discovery_map():
+    map_from_name_to_ip = dict()
+    devices = await Discover.discover()
+    for dev in devices.values():
+        await dev.update()
+        print(dev.host + " " + dev.alias)
+        map_from_name_to_ip.update({dev.alias: dev.host})
+    _super_user_config["name_map"] = map_from_name_to_ip
+
+
+if __name__ == "__main__":
+    asyncio.run(make_discovery_map())
+    asyncio.run(doit())
