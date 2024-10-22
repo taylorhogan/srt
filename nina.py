e437e2a995f96efcb4beee5dbdfada23980c242b
taylorhogan <taylor.hogan@gmail.com>
2024-10-22 10:52:21 -0400

    work on start/end stuff

---
+++ nina.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def start_nina():
+    print("Starting Nina")
+    subprocess.Popen[("C:/home/taylorhogan/Documents/tmh/nina.bat")]
+    print("Done with Nina")
+
+
+if __name__ == '__main__':
+    start_nina()
