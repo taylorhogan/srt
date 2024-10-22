60a1d331b5dcac9bb6593987d32c19604d18a3f4
taylorhogan <taylor.hogan@gmail.com>
2024-10-22 10:26:27 -0400

    work on start/end stuff

--- super_user_commands.py
+++ super_user_commands.py
@@ -2,11 +2,11 @@ import asyncio
 import os
 import subprocess
 import time
+import traceback
 from collections import OrderedDict
 
 import requests
 from kasa import Discover
-
 
 _super_user_config = OrderedDict(
     {
@@ -15,12 +15,14 @@ _super_user_config = OrderedDict(
         },
     })
 
-def make_discovery_map():
-    print ("make map")
+
+async def make_discovery_map():
+    traceback.print_stack()
+    print("make map")
     map_from_name_to_ip = dict()
     devices = await Discover.discover()
     for dev in devices.values():
-        #await dev.update()
+        # await dev.update()
         print(dev.host + " " + dev.alias)
         map_from_name_to_ip.update({dev.alias: dev})
     _super_user_config["name_map"] = map_from_name_to_ip
@@ -74,42 +76,35 @@ def shutdown_command():
 
 
 def start_nina():
-    print ("Starting Nina")
+    print("Starting Nina")
     subprocess.Popen[("C:/home/taylorhogan/Documents/tmh/nina.bat")]
-    print ("Done with Nina")
+    print("Done with Nina")
 
 
-_super_user_commands = {
-
-    "roof!": toggle_roof_command,
-    "lights_on!": kasa_lights_on(),
-    "lights_off!": kasa_lights_off(),
-    "nina!": start_nina()
-}
+def get_super_user_commands():
+    return {
+        "roof!": toggle_roof_command,
+        "lights_on!": kasa_lights_on(),
+        "lights_off!": kasa_lights_off(),
+        "nina!": start_nina()
+    }
 
 
 def do_super_user_command(words, account):
     config = _super_user_config
-    action = _super_user_commands.get(words[1], "no_key")
-    print (str(words))
+    action = get_super_user_commands().get(words[1], "no_key")
+    print(str(words))
     if action != "no_key":
         super_users = config["Super Users"]
         if account in super_users:
-            print ("Whoot")
+            print("Whoot")
             action()
             return True
         else:
-            #social_server.post_social_message(account + " Is not authorized\n")
-            print ("no auth")
+            # social_server.post_social_message(account + " Is not authorized\n")
+            print("no auth")
             return False
     else:
         return False
 
 
-def turn_inside_light(onoff):
-    if not _super_user_config.get("name_map"):
-        asyncio.run(make_discovery_map())
-    host = _super_user_config["name_map"]["Iris inside light"]
-    if host != None:
-        string = "kasa --host " + host + " " + onoff
-        os.system(string)
