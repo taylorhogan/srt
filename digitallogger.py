3b79167b95943576fb0b819882e4cab0f9cc2883
taylorhogan <taylor.hogan@gmail.com>
2024-11-14 12:43:49 -0500

    work on converting fits to jpgs

--- digitallogger.py
+++
@@ -1,8 +0,0 @@
-import sys
-import logging
-import time
-import paho.mqtt.publish as publish
-import ssl
-from pathlib import Path
-import requests
-import http.client, urllib
---
+++ fitstojpg.py
@@ -0,0 +1,68 @@
+from fnmatch import fnmatch
+
+from astropy.visualization import ZScaleInterval
+from astropy.io import fits
+from matplotlib import pyplot as plt
+import os
+from pathlib import Path
+import fnmatch
+
+
+def convert_to_jpg (in_file):
+
+    out_file=in_file.replace('.fits', '.jpg')
+    data = fits.getdata(in_file) # https://fits.gsfc.nasa.gov/samples/UITfuv2582gc.fits
+    zscale = ZScaleInterval()
+
+
+
+    plt.imshow(zscale(data), cmap="gray")
+    head, tail = os.path.split (out_file)
+    plt.title(tail)
+    plt.savefig(out_file)
+    return out_file
+
+def convert_dir (dir_name):
+    for cur, _dirs, files in os.walk(dir_name):
+       for candidate in fnmatch.filter(files, '*.fits'):
+           in_file = os.path.join(cur, candidate)
+           new_file = in_file.replace('.fits', '.jpg')
+           if not os.path.exists(new_file):
+                print(in_file)
+                convert_to_jpg(in_file)
+
+
+
+
+
+def get_latest_file(directory, extension):
+    latest_file = None
+    latest_time = 0
+
+    # Recursively search for files with the given extension
+    for file_path in Path(directory).rglob(f"*.{extension}"):
+        file_mod_time = file_path.stat().st_mtime
+        # Update latest file if this file is more recent
+        if file_mod_time > latest_time:
+            latest_time = file_mod_time
+            latest_file = file_path
+
+    return latest_file
+
+
+
+
+#
+# convert_dir ("/Users/taylorhogan/Desktop/NGC 6888/2024-09-11")
+# f = get_latest_file ("/Users/taylorhogan/Desktop/NGC 6888/2024-09-11", "fits")
+# print (f)
+
+
+
+
+
+
+
+
+#convert_to_jpg("base_images/2024-09-11_20-21-36_Ha_-19.90_300.00s_1x1_0030.fits")
+#convert_to_jpg("base_images/2024-11-14_00-51-54_R_1767_300.00s_1x1_0086.fits")
