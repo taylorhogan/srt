c7d937daf6fd0fff4a7c015225733b880a72d764
taylorhogan <taylor.hogan@gmail.com>
2025-01-07 10:06:52 -0500

    add crontab for start of server

--- config_public.py
+++ config_public.py
@@ -10,7 +10,7 @@ class PublicConfig():
         "install_location": '/Users/taylorhogan/Documents/tmh',
 
         "version": {
-            "date": "2025.1.3.01"
+            "date": "2025.1.7.01"
         },
 
 
