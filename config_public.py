c0680cf77cb23eaa033914c661fd10da22c714b5
taylorhogan <taylor.hogan@gmail.com>
2025-01-24 15:01:50 -0500

    work on auto pull on client side

--- config_public.py
+++ config_public.py
@@ -10,7 +10,7 @@ class PublicConfig():
         "install_location": '/Users/taylorhogan/Documents/tmh',
 
         "version": {
-            "date": "2025.1.24.06"
+            "date": "2025.1.24.07"
         },
 
         "logger": {
