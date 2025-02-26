d46dc848cba7ebb57dce43721bd16c4086757e3a
taylorhogan <taylor.hogan@gmail.com>
2025-02-26 11:54:03 -0500

    work on plot of dso against horizon

--- sortdsoobjects.py
+++ sortdsoobjects.py
@@ -2,22 +2,22 @@
 #   remove those that have been imaged, done
 #   how many hours will it be visible > some altitude
 # https://astroplan.readthedocs.io/en/stable/
+import datetime
+import json
+import operator
+import os
+
 import astropy.units as u
 import matplotlib.pyplot as plt
 import numpy as np
-import astroplan
-import datetime
+import pytz
 from astroplan import FixedTarget
 from astroplan import Observer
-from astroplan.plots import plot_finder_image
 from astropy.coordinates import EarthLocation
 from astropy.time import Time
-import os
-import config
 from matplotlib import dates
-import operator
-import pytz
 
+import config
 
 cfg = config.data()
 
@@ -29,15 +29,15 @@ def is_a_dso_object(name):
     except NameError:
         return None
 
-def get_horizon_from_azimuth (this_az, az, al):
-    for idx in range (len(az) - 1):
+
+def get_horizon_from_azimuth(this_az, az, al):
+    for idx in range(len(az) - 1):
         if this_az >= az[idx] and this_az <= az[idx + 1]:
             return al[idx]
     return al[-1]
 
 
-def find_alt_az_horizon_times (dso, my_observatory, observe_time):
-
+def find_alt_az_horizon_times(dso, my_observatory, observe_time):
     altitude = (my_observatory.altaz(observe_time, dso).alt) * (1 / u.deg)
 
     # Azimuth MUST be given to plot() in radians.
@@ -48,41 +48,50 @@ def find_alt_az_horizon_times (dso, my_observatory, observe_time):
 
     horizon = []
     az, al = map_az_to_horizon()
-    state=1
+
     start_time = None
     finish_time = None
+    start = observe_time[0]
+    local_tz = pytz.timezone('America/New_York')
+    utc_timezone = pytz.utc
+
+    end_of_dark_naive = my_observatory.twilight_morning_astronomical(Time(start), which='next').datetime
+    end_of_dark_utc = utc_timezone.localize(end_of_dark_naive)
+    end_of_dark_local = end_of_dark_utc.astimezone(local_tz)
+
+    start_of_dark_naive = my_observatory.twilight_evening_astronomical(Time(start), which='next').datetime
+    start_of_dark_utc = utc_timezone.localize(start_of_dark_naive)
+    start_of_dark_local = start_of_dark_utc.astimezone(local_tz)
+
 
     for idx in range(len(local_datetime)):
         h = get_horizon_from_azimuth(azimuth[idx], az, al)
         horizon.append(h)
-        if state == 1:
-            if  altitude[idx] >= h:
-                start_time = local_datetime[idx]
-                state = 2
-                continue
-        if state == 2:
-            if altitude[idx] < h:
+
+        if local_datetime[idx] > end_of_dark_local:
+            if finish_time is None:
                 finish_time = local_datetime[idx]
-                state =3
-                continue
+        else:
+            if local_datetime[idx] > start_of_dark_local:
+                if altitude[idx] >= h:
+                    finish_time = None
+                    if start_time is None:
+                        start_time = local_datetime[idx]
+                if altitude[idx] < h:
+                    if finish_time is None:
+                        finish_time = local_datetime[idx]
 
-
-
-
-    if state == 2:
-        finish_time = local_datetime[-1]
     elapsed_time = 0
     if start_time is not None:
         elapsed_time = finish_time - start_time
 
-    print (start_time, finish_time, elapsed_time)
-    return altitude, azimuth, horizon,start_time,finish_time,elapsed_time
+
+    return altitude, azimuth, horizon, start_time, finish_time, elapsed_time
 
 
-def plot_my_dso_and_horizon (dso, my_observatory, observe_time):
-
-
-    altitude, azimuth, horizon, start_time, finish_time, elapsed_time = find_alt_az_horizon_times(dso, my_observatory,observe_time)
+def plot_my_dso_and_horizon(dso, my_observatory, observe_time):
+    altitude, azimuth, horizon, start_time, finish_time, elapsed_time = find_alt_az_horizon_times(dso, my_observatory,
+                                                                                                  observe_time)
 
     masked_altitude = np.ma.array(altitude, mask=altitude < 0)
     local_datetime = my_observatory.astropy_time_to_datetime(observe_time)
@@ -90,13 +99,12 @@ def plot_my_dso_and_horizon (dso, my_observatory, observe_time):
     ax = plt.gca()
     style_kwargs = None
 
-
     local_tz = pytz.timezone('America/New_York')
     ax.plot(local_datetime, masked_altitude)
     ax.plot(local_datetime, horizon)
 
     ax.set_xlim([local_datetime[0], local_datetime[-1]])
-    date_formatter = dates.DateFormatter('%H',tz=local_tz)
+    date_formatter = dates.DateFormatter('%H', tz=local_tz)
     ax.xaxis.set_major_formatter(date_formatter)
     plt.setp(ax.get_xticklabels(), rotation=30, ha='right')
 
@@ -104,7 +112,7 @@ def plot_my_dso_and_horizon (dso, my_observatory, observe_time):
 
     start = local_datetime[0]
 
-    print(my_observatory.twilight_morning_astronomical(Time(start), which='next').datetime)
+
     twilights = [
         (my_observatory.sun_set_time(Time(start), which='next').datetime, 0.0),
         (my_observatory.twilight_evening_civil(Time(start), which='next').datetime, 0.1),
@@ -121,25 +129,24 @@ def plot_my_dso_and_horizon (dso, my_observatory, observe_time):
         ax.axvspan(twilights[i - 1][0], twilights[i][0],
                    ymin=0, ymax=1, color='grey', alpha=twi[1])
 
-
     ax.set_ylim([0, 90])
-
-
-
 
     # Set labels.
     ax.set_ylabel("Altitude")
     ax.set_xlabel("Time")
-    title = dso.name + "\n" + "Start: " + start_time.strftime("%H:%M") + " Finish: " + finish_time.strftime("%H:%M") + " Elapsed Time: " + str(elapsed_time)
+    if elapsed_time is not None:
+        title = dso.name + "\n" + "Start: " + start_time.strftime("%H:%M") + " Finish: " + finish_time.strftime(
+            "%H:%M") + " Elapsed Time: " + str(elapsed_time)
+    else:
+        title = dso.name + "Not Visible"
     plt.title(title)
-
-
 
     # Redraw figure for interactive sessions.
     ax.figure.canvas.draw()
 
     # Output.
     return ax
+
 
 def show_plots(dso):
     longitude = cfg["location"]["longitude"]
@@ -153,35 +160,29 @@ def show_plots(dso):
     now = datetime.datetime.now()
     sunset_tonight = my_observatory.sun_set_time(time, which='nearest')
     sunrise_tonight = my_observatory.sun_rise_time(time, which='nearest')
-    print(my_observatory.is_night(time))
-    print(sunset_tonight.iso)
-    print(sunrise_tonight.iso)
+
 
     object_is_up = my_observatory.target_is_up(time, dso)
-    print(object_is_up)
+
     observe_time = sunset_tonight
     observe_time = observe_time + np.linspace(-1, 14, 55) * u.hour
-
 
     dir_name = os.path.dirname(__file__)
     scratch_dir = os.path.join(dir_name + "/scratch")
     if not os.path.exists(scratch_dir):
         os.mkdir(scratch_dir)
 
-
-
-
-    #ax, hdu = plot_finder_image(dso, fov_radius=42*u.arcmin, reticle=True)
+    # ax, hdu = plot_finder_image(dso, fov_radius=42*u.arcmin, reticle=True)
 
     image_path = os.path.join(scratch_dir, "image.png")
-   # plt.savefig(image_path)
-   # plt.clf()
+    # plt.savefig(image_path)
+    # plt.clf()
 
-    #astroplan.plots.plot_sky(dso, my_observatory, observe_time)
-    #plt.legend(loc='center left', bbox_to_anchor=(1.25, 0.5))
+    # astroplan.plots.plot_sky(dso, my_observatory, observe_time)
+    # plt.legend(loc='center left', bbox_to_anchor=(1.25, 0.5))
     sky_path = os.path.join(scratch_dir, "sky.png")
-    #plt.savefig(sky_path)
-   #plt.clf()
+    # plt.savefig(sky_path)
+    # plt.clf()
 
     plot_my_dso_and_horizon(dso, my_observatory, observe_time)
     altitude_path = os.path.join(scratch_dir, "horizon.png")
@@ -189,20 +190,36 @@ def show_plots(dso):
     plt.savefig(altitude_path)
     plt.clf()
 
-
-
-
-
     return altitude_path, image_path, sky_path
 
 
-def map_az_to_horizon ():
+def get_above_horizon_time(dso):
+    longitude = cfg["location"]["longitude"]
+    latitude = cfg["location"]["latitude"]
+    elevation = cfg["location"]["elevation"]
+    observatory_name = cfg["location"]["observatory_name"]
+
+    location = EarthLocation.from_geodetic(longitude * u.deg, latitude * u.deg, elevation * u.m)
+    my_observatory = Observer(location=location, name=observatory_name, timezone="US/Eastern")
+    time = Time.now()
+    sunset_tonight = my_observatory.sun_set_time(time, which='nearest')
+    sunrise_tonight = my_observatory.sun_rise_time(time, which='nearest')
 
 
+    object_is_up = my_observatory.target_is_up(time, dso)
+
+    observe_time = sunset_tonight
+    observe_time = observe_time + np.linspace(-1, 14, 55) * u.hour
+    altitude, azimuth, horizon, start_time, finish_time, elapsed_time = find_alt_az_horizon_times(dso, my_observatory,
+                                                                                                  observe_time)
+    return elapsed_time
+
+
+def map_az_to_horizon():
     ax = plt.gca()
     data = []
-    az =[]
-    al =[]
+    az = []
+    al = []
     dir_name = os.path.dirname(__file__)
     file_path = os.path.join(dir_name + "/my.hrz")
     with open(file_path, 'r') as file:
@@ -211,13 +228,12 @@ def map_az_to_horizon ():
             columns = line.strip().split()
             if len(columns) == 2:
                 # Assuming the columns are numeric, convert to float
-                    col1 = float(columns[0])
-                    col2 = float(columns[1])
-                    az.append(col1)
-                    al.append(col2)
+                col1 = float(columns[0])
+                col2 = float(columns[1])
+                az.append(col1)
+                al.append(col2)
 
-
-    plt.plot (az, al)
+    plt.plot(az, al)
     ir_name = os.path.dirname(__file__)
     scratch_dir = os.path.join(dir_name + "/scratch")
     sky_path = os.path.join(scratch_dir, "h.png")
@@ -225,7 +241,16 @@ def map_az_to_horizon ():
     plt.clf()
     return az, al
 
+
 if __name__ == '__main__':
 
     obj = is_a_dso_object("ngc2903")
-    show_plots (obj)
+    show_plots(obj)
+    with open('instructions.json', 'r') as f:
+        instructions = json.load(f)
+    for instruction in instructions:
+        dso = instruction['dso']
+        obj = is_a_dso_object(dso)
+        if obj is not None:
+            elapsed_time = get_above_horizon_time(obj)
+            print (dso, str(elapsed_time))
