56671e1c370a8f9c0e61c34d3a82f7bb6e4e4554
taylorhogan <taylor.hogan@gmail.com>
2025-03-06 14:08:59 -0500

    work on ending sequences

--- start_social_server.bat
+++ start_social_server.bat
@@ -1,5 +1,5 @@
 @ECHO off
 CLS
 del C:\Users\iriso\Documents\development\iris\iris.log
-C:\Users\iriso\Documents\development\iris\venv\Scripts\python C:\Users\iriso\Documents\development\iris\social_server.py
+C:\Users\iriso\Documents\development\iris\venv\Scripts\python C:\Users\iriso\Documents\development\iris\social_server.py >>C:\Users\iriso\Documents\development\iris\logger.log
 EXIT /b 0
