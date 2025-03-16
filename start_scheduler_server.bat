80809cf25e121ac039d5514517c81566466e786d
taylorhogan <taylor.hogan@gmail.com>
2025-03-16 12:12:13 -0400

    work on parking scope

--- start_scheduler_server.bat
+++ start_scheduler_server.bat
@@ -1,6 +1,4 @@
 @ECHO off
 CLS
-del C:\Users\iriso\Documents\development\iris\iris.log
-del C:\Users\iriso\Documents\development\iris\logger.log
 C:\Users\iriso\Documents\development\iris\venv\Scripts\python C:\Users\iriso\Documents\development\iris\scheduler.py >>C:\Users\iriso\Documents\development\iris\logger.log
 EXIT /b 0
