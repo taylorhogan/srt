13d8a630e62b5c672b81dd257905c74109ee5c7a
taylorhogan <taylor.hogan@gmail.com>
2025-03-16 13:38:56 -0400

    work on parking scope

--- start_social_server.bat
+++ start_social_server.bat
@@ -1,4 +1,4 @@
 @ECHO off
 CLS
-C:\Users\iriso\Documents\development\iris\venv\Scripts\python C:\Users\iriso\Documents\development\iris\social_server.py >>C:\Users\iriso\Documents\development\iris\logger.log
+C:\Users\iriso\Documents\development\iris\venv\Scripts\python C:\Users\iriso\Documents\development\iris\social_server.py
 EXIT /b 0
