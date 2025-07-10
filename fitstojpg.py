d497c4ebf21954115228867c1b6373e2bf8cd324
taylorhogan <taylor.hogan@gmail.com>
2025-07-10 18:17:42 -0400

    work on clearing background of latest

--- fitstojpg.py
+++ fitstojpg.py
@@ -8,23 +8,6 @@ from pathlib import Path
 import fnmatch
 from PIL import Image
 
-def convert_to_jpg2(in_file):
-    out_file = in_file.replace('.fits', '.jpg')
-    with fits.open(in_file) as hdul:
-        # Get the image data from the first HDU
-        image_data = hdul[0].data
-        zscale = ZScaleInterval()
-        #image_data = zscale(image_data)
-
-        # Scale the image data to 0-255 for 8-bit image
-        image_data = (image_data - image_data.min()) / (image_data.max() - image_data.min()) * 255
-        image_data = image_data.astype('uint8')
-
-        # Create a PIL Image object
-        img = Image.fromarray(image_data)
-
-        # Save the image as a JPEG
-        img.save("foo.jpg")
 
 
 def convert_to_jpg(in_file):
@@ -33,6 +16,7 @@ def convert_to_jpg(in_file):
     data = fits.getdata(in_file)
     zscale = ZScaleInterval()
 
+    plt.figure(clear=True)
     plt.plot(zscale(data))
 
     plt.imshow(zscale(data), cmap="gray")
@@ -44,7 +28,6 @@ def convert_to_jpg(in_file):
     title = dso + "\n" + filename
     plt.title(title)
 
-    #plt.savefig("foo.jpg")
 
     plt.savefig(out_file)
     return out_file
@@ -74,9 +57,9 @@ def get_latest_file(directory, extension):
 
     return latest_file
 
-# convert_to_jpg("base_images/2024-09-11_20-21-36_Ha_-19.90_300.00s_1x1_0030.fits")
-#f = get_latest_file("/Users/taylorhogan/Desktop/NGC 6888/2024-09-11", "fits")
-#convert_to_jpg(str(f))
+convert_to_jpg("base_images/2024-09-11_20-21-36_Ha_-19.90_300.00s_1x1_0030.fits")
+f = get_latest_file("/Users/taylorhogan/Desktop/NGC 6888/2024-09-11", "fits")
+convert_to_jpg(str(f))
 
 #convert_to_jpg("base_images/2024-09-11_20-21-36_Ha_-19.90_300.00s_1x1_0030.fits")
 # convert_to_jpg("base_images/2024-11-14_00-51-54_R_1767_300.00s_1x1_0086.fits")
