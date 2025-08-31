353c8975fa6027711c9189d1168388f24cc37c82
taylorhogan <taylor.hogan@gmail.com>
2025-08-31 16:14:06 -0400

    defensive coding around getting kasa map

--- config_public.py
+++ config_public.py
@@ -7,7 +7,7 @@ class PublicConfig():
             "install_location": '/Users/taylorhogan/Documents/tmh'
         },
         "version": {
-            "date": "2025.8.28.1"
+            "date": "2025.8.31.1"
         },
 
         "logger": {
