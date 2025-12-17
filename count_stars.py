751bde2e47d57a12342069459f415feb27f61e69
taylorhogan <taylor.hogan@gmail.com>
2025-12-17 12:07:01 -0500

    work on observer2

--- count_stars.py
+++ count_stars.py
@@ -1,23 +1,20 @@
-
 import cv2 as cv
-import inside_camera_server
+
 import config
-import pushover
 import inside_camera_server
+import pushover
 
-def count_stars ():
 
+def count_stars():
     cfg = config.data()
 
     print("taking picture")
 
-
     to_path = cfg["camera safety"]["scope_view"]
-
 
     # Open camera with DirectShow backend (best for exposure on Windows)
     vid = cv.VideoCapture(0)  # Change 0 if you have multiple cameras
-    #vid = cv.VideoCapture(0, cv.CAP_DSHOW)
+    # vid = cv.VideoCapture(0, cv.CAP_DSHOW)
     # Optional: set resolution/FPS first (helps some cameras)
     vid.set(cv.CAP_PROP_FRAME_WIDTH, 3840)
     vid.set(cv.CAP_PROP_FRAME_HEIGHT, 2160)
@@ -25,30 +22,25 @@ def count_stars ():
     vid.set(cv.CAP_PROP_AUTOFOCUS, 1)
 
     # --- Set manual exposure here ---
-    #exposure_value = -6  # Try values from -1 (bright) to -11 (dark/short)
-    #vid.set(cv.CAP_PROP_EXPOSURE, exposure_value)
+    # exposure_value = -6  # Try values from -1 (bright) to -11 (dark/short)
+    # vid.set(cv.CAP_PROP_EXPOSURE, exposure_value)
 
     # Sometimes helps to also explicitly disable auto exposure (0.25 or 0.75 works on MSMF)
     vid.set(cv.CAP_PROP_AUTO_EXPOSURE, 0.25)
 
-
-
     pictures = []
     scores = []
-    for exposure_value in range(-11, 1, 1):
+    for exposure_value in range(-11, 5, 1):
         vid.set(cv.CAP_PROP_EXPOSURE, exposure_value)
         ret, frame = vid.read()
         if not ret:
             print("failed to read frame")
             return False
 
-
         score = inside_camera_server.best_exposure_score(frame)
         print(f"Exposure: {exposure_value} Score: {score}")
         pictures.append(frame)
         scores.append(score)
-
-
 
     best_score = max(scores)
     best_index = scores.index(best_score)
@@ -57,8 +49,8 @@ def count_stars ():
 
     pushover.push_message_with_picture("picture", to_path)
 
-
     print(f"best score:  {best_score} of: {scores}")
     return True
+
 
 count_stars()
