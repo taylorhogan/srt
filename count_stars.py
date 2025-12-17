85cfec859756b7f55f8ac80e3714ed448fa000b8
taylorhogan <taylor.hogan@gmail.com>
2025-12-17 11:57:07 -0500

    work on observer2

--- count_stars.py
+++ count_stars.py
@@ -25,42 +25,45 @@ def count_stars ():
     vid.set(cv.CAP_PROP_AUTOFOCUS, 1)
 
     # --- Set manual exposure here ---
-    exposure_value = -6  # Try values from -1 (bright) to -11 (dark/short)
-    vid.set(cv.CAP_PROP_EXPOSURE, exposure_value)
+    #exposure_value = -6  # Try values from -1 (bright) to -11 (dark/short)
+    #vid.set(cv.CAP_PROP_EXPOSURE, exposure_value)
 
     # Sometimes helps to also explicitly disable auto exposure (0.25 or 0.75 works on MSMF)
     # cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
 
+    ret, frame = vid.read()
+    if not ret:
+        print("failed to read frame")
+        return False
 
-
-
-
-
-    pictures = []
-    scores = []
-    for exposure_value in range(-1, 11, 1):
-        ret, frame = vid.read()
-        if not ret:
-            print("failed to read frame")
-            return False
-        vid.set(cv.CAP_PROP_EXPOSURE, exposure_value)
-
-        score = inside_camera_server.best_exposure_score(frame)
-        print(f"Exposure: {exposure_value} Score: {score}")
-        pictures.append(frame)
-        scores.append(score)
-
-
-
-    best_score = max(scores)
-    best_index = scores.index(best_score)
-    best_picture = pictures[best_index]
-    cv.imwrite(to_path, best_picture)
-
+    cv.imwrite(to_path, frame)
     pushover.push_message_with_picture("picture", to_path)
 
-
-    print(f"best score:  {best_score} of: {scores}")
-    return True
+    # pictures = []
+    # scores = []
+    # for exposure_value in range(-1, -11, -1):
+    #     ret, frame = vid.read()
+    #     if not ret:
+    #         print("failed to read frame")
+    #         return False
+    #     vid.set(cv.CAP_PROP_EXPOSURE, exposure_value)
+    #
+    #     score = inside_camera_server.best_exposure_score(frame)
+    #     print(f"Exposure: {exposure_value} Score: {score}")
+    #     pictures.append(frame)
+    #     scores.append(score)
+    #
+    #
+    #
+    # best_score = max(scores)
+    # best_index = scores.index(best_score)
+    # best_picture = pictures[best_index]
+    # cv.imwrite(to_path, best_picture)
+    #
+    # pushover.push_message_with_picture("picture", to_path)
+    #
+    #
+    # print(f"best score:  {best_score} of: {scores}")
+    # return True
 
 count_stars()
