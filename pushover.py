cdd21621b7de53616e40e6eb7caf09a79703e20f
taylorhogan <taylor.hogan@gmail.com>
2025-12-22 10:57:32 -0500

    work on imaging multiple targets in a night

--- pushover.py
+++ pushover.py
@@ -14,49 +14,44 @@ user = cfg['pushover']['user']
 
 
 def push_message(message):
-    conn = http.client.HTTPSConnection("api.pushover.net:443")
-    conn.request("POST", "/1/messages.json",
+    try:
+        conn = http.client.HTTPSConnection("api.pushover.net:443")
+        conn.request("POST", "/1/messages.json",
 
-                 urllib.parse.urlencode({
-                     "token": token,
-                     "user": user,
-                     "message": message,
-                     "verify": False,
-                 }), {"Content-type": "application/x-www-form-urlencoded", "verify": False})
-    output = conn.getresponse().read().decode('utf-8')
-    print(output)
+                     urllib.parse.urlencode({
+                         "token": token,
+                         "user": user,
+                         "message": message,
+                         "verify": False,
+                     }), {"Content-type": "application/x-www-form-urlencoded", "verify": False})
+        output = conn.getresponse().read().decode('utf-8')
+        print(output)
+    except Exception as e:
+        print(f"Error during pushover: {e}")
 
 
 def push_message_with_picture(message, image):
-    # conn = http.client.HTTPSConnection("api.pushover.net:443")
-    # conn.request("POST", "/1/messages.json",
-    #
-    #              urllib.parse.urlencode({
-    #                  "token": token,
-    #                  "user": user,
-    #                  "message": message,
-    #                  "verify": False,
-    #                  "attachment": ("image.jpg", open(image, "rb"), "image/jpeg")
-    #              }), {"Content-type": "application/x-www-form-urlencoded", "verify": False})
-    # output = conn.getresponse().read().decode('utf-8')
-    # print(output)
-    response = requests.post(
-        "https://api.pushover.net/1/messages.json",
-        data={
-            "token": token,
-            "user": user,
-            "message": message,  # Required: Your message text (up to 1024 chars)
-            "title": "News From Iris",  # Optional: Notification title (up to 250 chars)
-        },
-        files={
-            "attachment":
-                ("image.jpg",
-                 open(image, "rb"),
-                 "image/jpeg")
-        }
-    )
+    try:
 
-    print(response.text)
+        response = requests.post(
+            "https://api.pushover.net/1/messages.json",
+            data={
+                "token": token,
+                "user": user,
+                "message": message,  # Required: Your message text (up to 1024 chars)
+                "title": "News From Iris",  # Optional: Notification title (up to 250 chars)
+            },
+            files={
+                "attachment":
+                    ("image.jpg",
+                     open(image, "rb"),
+                     "image/jpeg")
+            }
+        )
+
+        print(response.text)
+    except Exception as e:
+        print(f"Error during pushover: {e}")
 
 
 def main():
