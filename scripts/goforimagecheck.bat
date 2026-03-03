516ca317d855d622a3fe74348278414b421ee3e0
taylor <taylor.hogan@gmail.com>
2026-03-03 09:09:41 -0500

    work on batch files

--- scripts/goforimagecheck.bat
+++ scripts/goforimagecheck.bat
@@ -1,4 +1,4 @@
 @ECHO off
 cd /d C:\Users\iriso\Documents\development\srt
-uv run C:\Users\iriso\Documents\development\srt\end_points\goforimagecheck.py
+uv run --python C:\Users\iriso\Documents\development\srt\end_points\goforimagecheck.py
 EXIT /b 0
