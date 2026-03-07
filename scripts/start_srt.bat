655567f2875fd8a656e5a3ee6ab83e4e5d623267
taylor <taylor.hogan@gmail.com>
2026-03-07 11:54:00 -0500

    work on command to create sequence

--- scripts/start_srt.bat
+++ scripts/start_srt.bat
@@ -1,4 +1,5 @@
   @ECHO off
   cd /d C:\Users\iriso\Documents\development\srt
+  SET ASTROPY_CACHE_DIR=C:\Users\iriso\Documents\development\srt\local\astropy_cache
   uv run end_points\start_srt.py
   EXIT /b 0
